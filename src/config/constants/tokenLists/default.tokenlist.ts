e8ff848881c27b7d136dd6608e5df2c1879c0119
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-01-01 20:29:00 +0100

    feat: Added default tokens

---
+++ src/config/constants/tokenLists/default.tokenlist.ts
@@ -0,0 +1,37 @@
+const defaultTokens = [
+    {
+        "name": "REKT Token",
+        "symbol": "REKT",
+        "address": "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+        "chainId": 137,
+        "decimals": 18,
+        "logoURI": "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+    },
+    {
+        "name": "WETH Token",
+        "symbol": "WETH",
+        "address": "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+        "chainId": 137,
+        "decimals": 18,
+        "logoURI": "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+    },
+    {
+        "name": "Ethereum Token",
+        "symbol": "ETH",
+        "address": "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+        "chainId": 137,
+        "decimals": 18,
+        "logoURI": "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+    },
+    {
+        "name": "USDC Token",
+        "symbol": "USDC",
+        "address": "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+        "chainId": 137,
+        "decimals": 18,
+        "logoURI": "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
+    },
+]
+
+
+export default defaultTokens;
