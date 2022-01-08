9e6e1cca250f27c377076d555f1cef954e7b4859
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-01-08 17:35:03 +0100

    fix: fixed duplicate warnings
    corrected duplicate warnings when switching to an unsupported chains

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import {
   Container, Flex,
 } from '@chakra-ui/layout';
-import { useToast } from '@chakra-ui/react';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import { FC, useEffect } from 'react';
 import { Header } from './components/Header/Header';
 import { Main } from './components/Main/Main';
-import { Web3Error } from './Errors/Web3Error';
+import { useWalletHandleError } from './hooks/useWalletHandleError';
 import { useEagerConnect, useInactiveListener } from './hooks/useWeb3'
 import { useUserStore } from './stores/UserStore';
 
 const App: FC = () => {
 
   const { setActivatingConnector, activatingConnector } = useUserStore();
-  const { connector, error } = useWeb3React<Web3Provider>()
+  const { connector } = useWeb3React<Web3Provider>();
 
   useEffect(() => {
     if (activatingConnector && activatingConnector === connector) {
@@ -27,22 +26,7 @@ const App: FC = () => {
 
   // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
   useInactiveListener(!triedEager || !!activatingConnector)
-
-  const toast = useToast({
-    title: 'Error',
-    duration: 2_500,
-    status: 'error',
-  });
-
-  useEffect(() => {
-    if (error) {
-      const currentError = new Web3Error(error);
-      toast({
-        description: currentError.message
-      });
-    }
-    
-  }, [error])
+  useWalletHandleError();
 
   return (
     <Container maxW="100vw" p={0} >
---
+++ src/hooks/useWalletHandleError.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import { useToast } from '@chakra-ui/react';
+import { useWeb3React } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3Error } from '../Errors/Web3Error';
+
+const useWalletHandleError = () => {
+
+    const { error } = useWeb3React<Web3Provider>();
+    const [errorWeb3, setErrorWeb3] = useState<Web3Error | undefined>(undefined);
+
+    const toast = useToast({
+        title: 'Error',
+        duration: 2_500,
+        status: 'error',
+    });
+
+    useEffect(() => {
+        if (error) {
+            if (errorWeb3 === undefined) {
+                setErrorWeb3(new Web3Error(error));
+            }
+        }
+    }, [error])
+
+    useEffect(() => {
+        if (errorWeb3) {
+            toast({
+                description: errorWeb3.message
+            })
+            setTimeout(() => {
+                setErrorWeb3(undefined);
+            }, 2000)
+        }
+    }, [errorWeb3])
+}
+
+export { useWalletHandleError };
