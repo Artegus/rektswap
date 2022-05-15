4066afb198cbe2e6aa82d122371f5ec8f3d24808
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-05-15 23:22:16 +0200

    feat: added default contracts

---
+++ src/config/constants/tokenLists/default.contracts.ts
@@ -0,0 +1,18 @@
+export const defaultContracts = {
+    WETH: {
+        name: "WETH",
+        address: "0xd0A1E359811322d97991E03f863a0C30C2cF029C"
+    },
+    REKT_COIN: {
+        name: "REKT",
+        address: "0x33632E8362e9084F36Ab68BF59269F54BDe787AE"
+    },
+    REKT_TRANSACTION_BATCHER: {
+        name: "RektTransactionBatcher",
+        address: "0x4b170fA8934740a3a753E2C7cfD2Dd1D973FaC93"
+    },
+    UNISWAPV2_ROUTER02: {
+        name: "UniswapV2Router02",
+        address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+    }
+}
