75b81e80841656afc979f004da5f03d41a7aabe1
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-05-16 13:56:09 +0200

    refactor: created array of tabs data

--- src/components/Swap/ActionsTabs.tsx
+++ src/components/Swap/ActionsTabs.tsx
@@ -1,10 +1,26 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
+import { FC } from "react";
+import { Props } from "../../types/TabProps/TabProps";
 import { SellRektTab } from "./SellRektTab";
-import { SwapTab } from "./SwapTab";
+import { BuyRektTab } from "./BuyRektTab";
+
+type TabData = {
+    title: string;
+    content: FC<Props>
+}
 
 export const ActionsTabs = () => {
 
-    const actionTabs = ["Buy REKTcoin", "Sell REKTcoin"];
+    const tabsData: TabData[] = [
+        {
+            title: "Buy REKTcoin",
+            content: BuyRektTab
+        },
+        {
+            title: "Sell REKTcoin",
+            content: SellRektTab
+        },
+    ]
 
     return (
         <Tabs
@@ -13,17 +29,16 @@ export const ActionsTabs = () => {
             borderWidth='1px'
         >
             <TabList mb='1em'>
-                {actionTabs.map(action => (
-                    <Tab key={action} >{action}</Tab>
+                {tabsData.map((tab, index) => (
+                    <Tab key={index} >{tab.title}</Tab>
                 ))}
             </TabList>
             <TabPanels>
-                <TabPanel>
-                    <SwapTab tabTitle={actionTabs[0]} />
-                </TabPanel>
-                <TabPanel>
-                    <SellRektTab tabTitle={actionTabs[1]} />
-                </TabPanel>
+                {tabsData.map((tab, index) => (
+                    <TabPanel key={index} >
+                        <tab.content tabTitle={tab.title} />
+                    </TabPanel>
+                ))}
             </TabPanels>
         </Tabs>
     )
