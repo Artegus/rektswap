4b0ba7ecfd9f87388252fd45606f90d1cc329cfa
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-04 17:26:22 +0200

    fix: load chain id from .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,4 @@
-let chainId;
-
-if (process.env.NODE_ENV === 'development') {
-    chainId = '42'
-} else if (process.env.NODE_ENV === 'production') {
-    chainId = process.env.REACT_APP_CHAIN_ID
-}
-
 export const config = {
-    CHAIN_ID : Number(chainId),
-    ENVIROMENT: process.env.NODE_ENV
+    CHAIN_ID : Number(process.env.REACT_APP_CHAIN_ID),
+    ENVIROMENT: process.env.NODE_ENV,
 }
