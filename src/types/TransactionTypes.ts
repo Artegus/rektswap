d38bd16c5c4b1e39f44cdb4f17afeabad4339b59
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-05 01:26:57 +0200

    feat: added transaction types from ethers

---
+++ src/types/TransactionTypes.ts
@@ -0,0 +1,5 @@
+import { providers } from "ethers";
+
+export type TransactionReceipt = providers.TransactionReceipt;
+export type TransactionResponse = providers.TransactionResponse;
+export type TransactionRequest = providers.TransactionRequest;
