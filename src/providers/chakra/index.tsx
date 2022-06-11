236cd9fe2e9576648dfe3a3622a6979114368890
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-12 00:55:57 +0200

    feat: added custom theme

--- src/providers/chakra/index.tsx
+++ src/providers/chakra/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from './theme/theme'
 
 const ChakraUI: React.FC = ({ children }) => {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme} >
             { children }
         </ChakraProvider>
     )
