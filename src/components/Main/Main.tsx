ccbd64766dd6d1918f5f9a9df574c3ba67839b13
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-12 00:54:04 +0200

    refactor: responsive design adaptation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,6 @@
-import React from 'react';
 import { FC } from 'react';
 
-import {
-	Box,
-	Flex, 
-	HStack, 
-	SimpleGrid,
-	Center, 
-	Grid,
-	Container,
-	Heading,
-	Text
-} from '@chakra-ui/layout';
+import { Flex } from '@chakra-ui/layout';
 
 import { FaqHeader as MainHeader } from '../Faq/FaqHeader';
 import { MainTokenPresentation } from './MainTokenPresentation';
@@ -21,23 +10,20 @@ import { Roadmap } from '../Roadmap/Roadmap';
 export const Main: FC = () => {
 	return (
 		<Flex
-			h={{ base: '100vh' }}
 			py={2}
 			px={5}
 			direction={'column'}
 		>
 			<MainHeader />
 			<MainTokenPresentation />
-			<Center>
-				<Flex 
-					direction={['column', 'column','row']} 
-					gap={[10, 10, 10, 10, 20]}
-					mx={[0, 0, 0, '10%', '15%']}
-				>
-					<FaqForTheMainPage />
-					<Roadmap />
-				</Flex>
-			</Center>
+			<Flex
+				direction={['column', 'column', 'row']}
+				gap={[10, 10, 10, 10, 20]}
+				mx={[0, 0, 0, '10%', '15%']}
+			>
+				<FaqForTheMainPage />
+				<Roadmap />
+			</Flex>
 		</Flex>
 	);
 }
