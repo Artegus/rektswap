23c7f16b4e81e99c6b9a1006185d92a9350ce4e7
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-12 00:51:33 +0200

    refactor: added chakra text props

--- src/components/Logo/RektText.tsx
+++ src/components/Logo/RektText.tsx
@@ -1,26 +1,26 @@
 import { FC } from 'react';
 import {
-    Text, Heading
+    Text, Heading, TextProps
 } from '@chakra-ui/react';
 
 export const RektText: FC<{
 	asHeading?: boolean,
-	fontSize?: string
-}> = ({asHeading = false, fontSize='xl'}) => {
+	otherProps?: TextProps
+}> = ({asHeading = false, otherProps}) => {
 	if(asHeading) return (
 		<Heading
 			bgGradient='linear(270deg, #ff3737, #8247E5)'
 			backgroundClip='text'
-			fontSize={fontSize}
 			fontFamily={`'Kdam Thmor Pro', sans-serif`}
+			{...otherProps}
 		>REKT</Heading>
 	);
 	else return (
 		<Text
 			bgGradient='linear(270deg, #ff3737, #8247E5)'
 			backgroundClip='text'
-			fontSize={fontSize}
 			fontFamily={`'Kdam Thmor Pro', sans-serif`}
+			{...otherProps}
 		>REKT</Text>
 	);
 
