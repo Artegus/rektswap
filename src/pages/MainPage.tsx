adc5b7c75f50fb7a50c5fc8620ae2b59044f448a
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-12 01:57:57 +0200

    fix: unused imports

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
 import { FC } from 'react';
-
-import {
-	Box
-} from '@chakra-ui/layout';
-
 import { Main } from '../components/Main/Main';
+
+
 
 export const MainPage: FC = () => {
 	return (
