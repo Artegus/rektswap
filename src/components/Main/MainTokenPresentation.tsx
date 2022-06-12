8ab9c6a0d9cfe843cacb16658284afe22a6a56ae
Dennis <39626597+DennisDv24@users.noreply.github.com>
2022-06-12 15:12:41 +0000

    feat: parallax and more (#50)
    * updated dev links
    
    * color animation
    
    * fix: light theme color animation fix

--- src/components/Main/MainTokenPresentation.tsx
+++ src/components/Main/MainTokenPresentation.tsx
@@ -1,13 +1,29 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import {
 	Heading,
-	Flex, Box, HStack
-} from '@chakra-ui/layout';
+	Flex, Box, HStack,
+	Text, useColorMode
+} from '@chakra-ui/react';
 
 import { RektText } from '../Logo/RektText';
+import { 
+	useViewportScroll,
+	useTransform,
+	motion 
+} from 'framer-motion';
 
 export const MainTokenPresentation: FC = () => {
+
+    const { colorMode } = useColorMode();
+	const { scrollY } = useViewportScroll();
+
+	const titleY = useTransform(scrollY, [0, 500], [0, 100]);
+	const titleOpacity = useTransform(scrollY, [0, 300], [1, 0]);
+	const secondaryTitleColor = useTransform(
+		scrollY, [0, 300], [colorMode === 'light' ? '#000' : '#fff', '#8247E5']
+	);
+	
 	return (
 		<Flex
 			minH="100vh"
@@ -19,20 +35,31 @@ export const MainTokenPresentation: FC = () => {
 			alignItems='center'
 			px={['10vw', '10vw', '10vw', '25vw', '30vw']}
 		>
-			<RektText
-				asHeading
-				otherProps={{
-					textAlign: 'center',
-					fontSize: { base: '4xl', md: '5xl', lg: '6xl' },
-					flex: 2
+			<motion.div 
+				className='title' 
+				style={{
+					y: titleY,
+					zIndex: -1,
+					opacity: titleOpacity
 				}}
-			/>
+			>
+				<RektText
+					asHeading
+					otherProps={{
+						textAlign: 'center',
+						fontSize: { base: '6xl', md: '7xl', lg: '9xl' },
+						flex: 2,
+					}}
+				/>
+			</motion.div>
 			<Heading
 				color='1a202c'
 				textAlign='left'
 				fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}
 			>
-				REKT is an experimental shitcoin
+				<motion.div style={{color:secondaryTitleColor, display: 'inline'}}>
+					REKT
+				</motion.div>coin is an experimental shitcoin
 				that randomly deflates using Chainlink
 				random numbers.
 			</Heading>
