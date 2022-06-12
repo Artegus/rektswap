29984780f4e4efb5b9078ba70c33b4a53e370476
DennisDv24 <jokarboxing@gmail.com>
2022-06-12 16:55:47 +0200

    color animation

--- src/components/Main/MainTokenPresentation.tsx
+++ src/components/Main/MainTokenPresentation.tsx
@@ -1,13 +1,25 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import {
 	Heading,
-	Flex, Box, HStack
+	Flex, Box, HStack,
+	Text
 } from '@chakra-ui/layout';
 
 import { RektText } from '../Logo/RektText';
+import { 
+	useViewportScroll,
+	useTransform,
+	motion 
+} from 'framer-motion';
 
 export const MainTokenPresentation: FC = () => {
+	
+	const { scrollY } = useViewportScroll();
+	const titleY = useTransform(scrollY, [0, 500], [0, 100]);
+	const titleOpacity = useTransform(scrollY, [0, 300], [1, 0]);
+	const secondaryTitleColor = useTransform(scrollY, [0, 300], ['#fff', '#8247E5']);
+	
 	return (
 		<Flex
 			minH="100vh"
@@ -19,20 +31,31 @@ export const MainTokenPresentation: FC = () => {
 			alignItems='center'
 			px={['10vw', '10vw', '10vw', '25vw', '30vw']}
 		>
-			<RektText
-				asHeading
-				otherProps={{
-					textAlign: 'center',
-					fontSize: { base: '6xl', md: '7xl', lg: '8xl' },
-					flex: 2
+			<motion.div 
+				className='title' 
+				style={{
+					y: titleY,
+					zIndex: -1,
+					opacity: titleOpacity
 				}}
-			/>
+			>
+				<RektText
+					asHeading
+					otherProps={{
+						textAlign: 'center',
+						fontSize: { base: '6xl', md: '7xl', lg: '9xl' },
+						flex: 2,
+					}}
+				/>
+			</motion.div>
 			<Heading
 				color='1a202c'
 				textAlign='left'
 				fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}
 			>
-				REKTcoin is an experimental shitcoin
+				<motion.div style={{color:secondaryTitleColor, display: 'inline'}}>
+					REKT
+				</motion.div>coin is an experimental shitcoin
 				that randomly deflates using Chainlink
 				random numbers.
 			</Heading>
