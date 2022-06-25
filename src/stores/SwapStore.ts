1f149119ca547978dc384e84a68c800ad6188330
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-26 00:52:28 +0200

    fix: refactor swap store

--- src/stores/SwapStore.ts
+++ src/stores/SwapStore.ts
@@ -1,78 +1,65 @@
 import create from 'zustand';
-import { IToken } from '../types/IToken';
 
 interface SwapStore {
-    tokenIn: IToken | undefined;
-    tokenOut: IToken | undefined;
-    typedIn: string;
-    estimatedOut: string;
     approvedContract: boolean;
-    setTypedIn: (amout: string) => void;
-    setEstimitedOut: (amout: string) => void;
-    setTokenIn: (_tokenIn: IToken | undefined) => void;
-    setTokenOut: (_tokenOut: IToken | undefined) => void;
-    turnAroundTokens: () => void;
-	lastTx: object;
-	setLastTx: (tx: object) => void;
-	currentTab: "Buy" | "Sell";
-	currentTabIsBuy: () => void;
-	currentTabIsSell: () => void;
+    ethBalance: string;
+    rektBalance: string;
+    swap: {
+        inputAmount: string;
+        outputAmount: string;
+    }
     setApprovedContract: (approved: boolean) => void;
+    setEthBalance: (value: string) => void;
+    setRektBalance: (value: string) => void;
+    setBuyInputAmount: (amount: string) => void;
+    setBuyOutputAmount: (amount: string) => void;
 }
 
 
-const useSwapStore = create<SwapStore>((set, get) => ({
-    tokenIn: undefined,
-    tokenOut: undefined,
-    typedIn: '',
-    estimatedOut: '',
-	lastTx: {},
-	currentTab: "Buy",
+const useSwapStore = create<SwapStore>((set, _get) => ({
     approvedContract: false,
+    ethBalance: '',
+    rektBalance: '',
+    swap : {
+        inputAmount: '',
+        outputAmount: '',
+    },
     setApprovedContract: (approved: boolean) => {
         return set((state) => ({
             ...state, approvedContract: approved
         }))
     },
-    setTokenIn: (_tokenIn: IToken | undefined ) => {
-        if (get().tokenOut === undefined) {
-            return set((state) => ({ ...state, tokenIn: _tokenIn }));
-        }
-        if (_tokenIn?.address === get().tokenOut?.address) {
-            return set((state) => ({ ...state, tokenIn: get().tokenOut, tokenOut: get().tokenIn}));
-        }
-        
-        return set((state) => ({ ...state, tokenIn: _tokenIn }));
-    },
-    setTokenOut: (_tokenOut: IToken | undefined) => {
-        if (_tokenOut?.address !== get().tokenIn?.address) {
-            return set((state) => ({ ...state, tokenOut: _tokenOut }));
-        }
-    },
-    turnAroundTokens: () => {
-        return set((state) =>({
+    setEthBalance: (value) => {
+        return set((state) => ({
             ...state,
-            tokenIn: get().tokenOut,
-            tokenOut: get().tokenIn,
-            estimatedOut: get().typedIn,
-            typedIn: get().estimatedOut
-        }));
+            ethBalance: value
+        }))
     },
-    setEstimitedOut: (amout: string) => {
-        return set((state) => ({ ...state, estimatedOut: amout }));
+    setRektBalance: (value) => {
+        return set((state) => ({
+            ...state,
+            rektBalance: value
+        }))
     },
-    setTypedIn: (amout: string) => { 
-        return set((state) => ({ ...state, typedIn: amout}))
+    setBuyInputAmount: (amount) => {
+        return set((state) => ({
+            ...state,
+            swap: {
+                ...state.swap,
+                inputAmount: amount
+            }
+        }))
     },
-	setLastTx: (tx: object) => {
-		return set(state => ({ ...state, lastTx: tx }))
-	},
-	currentTabIsBuy: () => {
-		return set(state => ({ ...state, currentTab: "Buy" }))
-	},
-	currentTabIsSell: () => {
-		return set(state => ({ ...state, currentTab: "Sell" }))
-	}
+    setBuyOutputAmount: (amount) => {
+        return set((state) => ({
+            ...state,
+            swap: {
+                ...state.swap,
+                outputAmount: amount
+            }
+        }))
+    }
+
 }));
 
 export { useSwapStore };
