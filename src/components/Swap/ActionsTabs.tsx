a52008a88c7a6c13464791a95af117350eb17047
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2022-06-25 19:40:47 +0200

    fix: removed unused swap store

--- src/components/Swap/ActionsTabs.tsx
+++ src/components/Swap/ActionsTabs.tsx
@@ -1,24 +1,22 @@
+import { FC } from "react";
 import { 
 	Tab, TabList, TabPanel, TabPanels, Tabs,
 	useColorModeValue,
 	VStack, useColorMode
 } from "@chakra-ui/react"
-import { FC } from "react";
 import { Props } from "../../types/TabProps/TabProps";
 import { SellRektTab } from "./SellRektTab";
 import { BuyRektTab } from "./BuyRektTab";
-import { useSwapStore } from '../../stores/SwapStore';
 import { ACTION_TABS } from "./responsive/breakpoints";
 
 type TabData = {
     title: string;
     content: FC<Props>;
-	handleClick: () => void;
 }
 
 export const ActionsTabs = () => {
+
 	const { colorMode } = useColorMode();
-	const { currentTabIsBuy, currentTabIsSell } = useSwapStore();
 
 	const borderColor = useColorModeValue('#E6DAFA', '#1a263c');
 	const colorScheme = useColorModeValue('purple', 'purple');
@@ -28,12 +26,10 @@ export const ActionsTabs = () => {
         {
             title: "Buy REKTcoin",
             content: BuyRektTab,
-			handleClick: currentTabIsBuy
         },
         {
             title: "Sell REKTcoin",
             content: SellRektTab,
-			handleClick: currentTabIsSell
         },
     ]
 
@@ -52,7 +48,7 @@ export const ActionsTabs = () => {
 			>
 				<TabList mb='1em'>
 					{tabsData.map((tab, index) => (
-						<Tab key={index} onClick={tab.handleClick}>{tab.title}</Tab>
+						<Tab key={index} >{tab.title}</Tab>
 					))}
 				</TabList>
 				<TabPanels>
